984ff29014d61385c848abf05ed14bae90a20781
Le Chien <lhc021120@gmail.com>
2026-01-08 09:59:59 +0700

    update folder

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Loading from "./components/common/Loading";
+
+function App() {
+  return (
+    <>
+      <Loading />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/common/Loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="w-full h-full grid place-items-center flex-1">
+      <div className="loader"></div>
+    </div>
+  );
+}
